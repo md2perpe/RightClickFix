1ac7df76e65561f94c442d74ab40ffe6f80c1698
Per Persson <md2perpe+github@gmail.com>
2018-04-26 05:47:42 +0000

    Remove temporary variable

--- src/extension.ts
+++ src/extension.ts
@@ -4,15 +4,13 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 
-    let newFile = vscode.commands.registerCommand('rcf.newFile', () => {
+    context.subscriptions.push(vscode.commands.registerCommand('rcf.newFile', () => {
         vscode.commands.executeCommand('explorer.newFile');
     });
-    context.subscriptions.push(newFile);
 
-    let newFolder = vscode.commands.registerCommand('rcf.newFolder', () => {
+    context.subscriptions.push(vscode.commands.registerCommand('rcf.newFolder', () => {
         vscode.commands.executeCommand('explorer.newFolder');
     });
-    context.subscriptions.push(newFolder);
 
 }
 

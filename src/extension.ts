5b769dd313c19d3270b492a910a317be0cc5acda
Per Persson <md2perpe+github@gmail.com>
2018-04-26 05:49:44 +0000

    Use only one push call

--- src/extension.ts
+++ src/extension.ts
@@ -3,15 +3,17 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-
-    context.subscriptions.push(vscode.commands.registerCommand('rcf.newFile', () => {
-        vscode.commands.executeCommand('explorer.newFile');
-    });
-
-    context.subscriptions.push(vscode.commands.registerCommand('rcf.newFolder', () => {
-        vscode.commands.executeCommand('explorer.newFolder');
-    });
-
+    context.subscriptions.push(
+        
+        vscode.commands.registerCommand('rcf.newFile', () => {
+            vscode.commands.executeCommand('explorer.newFile');
+        }),
+        
+        vscode.commands.registerCommand('rcf.newFolder', () => {
+            vscode.commands.executeCommand('explorer.newFolder');
+        })
+        
+    );
 }
 
 export function deactivate() {
